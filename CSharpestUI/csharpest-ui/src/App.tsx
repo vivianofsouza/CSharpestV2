3ddd21d3c4eaf593d1e5ef347f33f7c3f13becec
vivianofsouza <vdsouza@email.sc.edu>
2023-11-16 18:36:33 -0500

    adding very basic navigation

--- CSharpestUI/csharpest-ui/src/App.tsx
+++ CSharpestUI/csharpest-ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import axios from "axios";
+import Nav from "react-bootstrap/Nav"; // Using bootstrap, pre-made HTML components for React projects. import components one by one as needed
 import "./App.css";
 
 function App() {
---
+++ CSharpestUI/csharpest-ui/src/pages/Checkout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import Nav from "react-bootstrap/Nav"; // Using bootstrap, pre-made HTML components for React projects. import components one by one as needed
+import "../App.css";
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  );
+}
+
+export default Checkout;
