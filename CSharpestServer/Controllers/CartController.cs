7d33483177a2c88935a96010acec339cc9d34eba
vivianofsouza <vdsouza@email.sc.edu>
2023-12-03 20:56:01 -0500

    returning sorted list by price from ItemController

--- CSharpestServer/Controllers/CartController.cs
+++ CSharpestServer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CSharpestServer.Classes;
-using CSharpestServer.Services.phase1;
+using CSharpestServer.Services;
+using CSharpestServer.Services.Interfaces;
 
 //	Last modified by: Vivian D'Souza
 //	Windows Prog 547
@@ -10,89 +11,92 @@ namespace CSharpestServer.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class CartController : ControllerBase
+    public class CartController : Controller
     {
-        InventoryLoader inventoryLoader = new InventoryLoader(@".\data\inventory.json");
-        UserLoader userLoader = new UserLoader(@".\data\users.json");
-        UserWriter userWriter = new UserWriter(@".\data\users.json");
+        private readonly ICartService _cartService;
+
+        //InventoryLoader inventoryLoader = new InventoryLoader(@".\data\inventory.json");
+        //UserLoader userLoader = new UserLoader(@".\data\users.json");
+        //UserWriter userWriter = new UserWriter(@".\data\users.json");
         Guid currUserID = new Guid("c4f9f3c1-9aa1-4d72-8a4c-4e03549e5bc1");
 
-        public CartController()
+        public CartController(CartService cartService)
         {
+            this._cartService = cartService;
         }
 
         // GET: api/<CartController>
-        [HttpGet("GetCartItems")]
-        public List<CartItem> GetCartItems(Guid UserID)
-        {
-            List<Shopper> users = userLoader.loadUsers();
-            Shopper user = users.Find(x => x.AccountID == currUserID);
+        //[HttpGet("GetCartItems")]
+        //public List<CartItem> GetCartItems(Guid UserID)
+        //{
 
-            if (user == null)
-            {
-                user = new Shopper("Example", "User", "exampleuser@email.com", "ExamplePW", "phone", "address", new Cart());
-            }
+        //    List<Shopper> users = userLoader.loadUsers();
+        //    Shopper user = users.Find(x => x.AccountID == currUserID);
 
-            List<CartItem> cartItems = user.Cart.Items;
-            return cartItems;
+        //    if (user == null)
+        //    {
+        //        user = new Shopper("Example", "User", "exampleuser@email.com", "ExamplePW", "phone", "address", new Cart());
+        //    }
 
-        }
+        //    List<CartItem> cartItems = user.Cart.Items;
+        //    return cartItems;
+        //}
 
         [HttpPost("AddItemToCart")]
         public void AddItemToCart([FromForm] Guid ItemID, [FromForm] int quantity)
         {
-            List<Item> items = inventoryLoader.loadInventory();
-            //Has been modified to not concern itself with getting current user back from frontend
-            //currUserID has been hard coded for phase 1
-            List<Shopper> users = userLoader.loadUsers();
+            //List<Item> items = inventoryLoader.loadInventory();
+            ////Has been modified to not concern itself with getting current user back from frontend
+            ////currUserID has been hard coded for phase 1
+            //List<Shopper> users = userLoader.loadUsers();
 
-            Shopper user = users.Find(x => x.AccountID == currUserID); // get user from database using id
-            //ensure user was found
-            if (user == null) { Environment.Exit(0); }
+            //Shopper user = users.Find(x => x.AccountID == currUserID); // get user from database using id
+            ////ensure user was found
+            //if (user == null) { Environment.Exit(0); }
 
-            Item item = items.Find(x => x.ItemId == ItemID); // get item from database using id
-            //ensure item was found
-            if (item == null) { Environment.Exit(0); }
+            //Item item = items.Find(x => x.ItemId == ItemID); // get item from database using id
+            ////ensure item was found
+            //if (item == null) { Environment.Exit(0); }
 
-            //Create totalPrice of product
-            decimal totalPrice = item.Price * (decimal)quantity;
+            ////Create totalPrice of product
+            //decimal totalPrice = item.Price * (decimal)quantity;
 
-            //Adds x number of item y to cart
-            if (user.Cart != null && quantity > 0 && item.Stock >= quantity)
-            {
+            ////Adds x number of item y to cart
+            //if (user.Cart != null && quantity > 0 && item.Stock >= quantity)
+            //{
 
-                if (user.Cart.Items.Find(x => x.Item.ItemId == ItemID) == null)
-                {
-                    //First instance of this item being in cart
-                    //Create CartItem
-                    CartItem cartItem = new CartItem(item, quantity, totalPrice);
-                    user.Cart.Items.Add(cartItem);
-                    user.Cart.Subtotal += cartItem.TotalPrice;
-                    userWriter.writeUser(user);
+            //    if (user.Cart.Items.Find(x => x.Item.ItemId == ItemID) == null)
+            //    {
+            //        //First instance of this item being in cart
+            //        //Create CartItem
+            //        CartItem cartItem = new CartItem(item, quantity, totalPrice);
+            //        user.Cart.Items.Add(cartItem);
+            //        user.Cart.Subtotal += cartItem.TotalPrice;
+            //        userWriter.writeUser(user);
 
-                }
-                else
-                {   // make sure user cannot add more than (stock minus what's already in cart)
-                    if ((user.Cart.Items.Single(x => x.Item.ItemId == ItemID).Quantity + quantity) <= item.Stock)
-                    {
-                        //Customer is adding more of this item to cart
-                        user.Cart.Items.Single(x => x.Item.ItemId == ItemID).Quantity += quantity;
-                        user.Cart.Items.Single(x => x.Item.ItemId == ItemID).TotalPrice += totalPrice;
-                        user.Cart.Subtotal += quantity * item.Price;
-                        userWriter.writeUser(user);
-                    }
-                }
+            //    }
+            //    else
+            //    {   // make sure user cannot add more than (stock minus what's already in cart)
+            //        if ((user.Cart.Items.Single(x => x.Item.ItemId == ItemID).Quantity + quantity) <= item.Stock)
+            //        {
+            //            //Customer is adding more of this item to cart
+            //            user.Cart.Items.Single(x => x.Item.ItemId == ItemID).Quantity += quantity;
+            //            user.Cart.Items.Single(x => x.Item.ItemId == ItemID).TotalPrice += totalPrice;
+            //            user.Cart.Subtotal += quantity * item.Price;
+            //            userWriter.writeUser(user);
+            //        }
+            //    }
 
-            }
-            else
-            {
+            //}
+            //else
+            //{
 
-                if (user.Cart == null) { Environment.Exit(0); } // User added nothing to cart
+            //    if (user.Cart == null) { Environment.Exit(0); } // User added nothing to cart
 
-                if (quantity < 0) { Environment.Exit(0); } // Should never happen but can't hurt
+            //    if (quantity < 0) { Environment.Exit(0); } // Should never happen but can't hurt
 
-                if (item.Stock < quantity) { Environment.Exit(0); } // Not enough in stock to purchase that amount
-            }
+            //    if (item.Stock < quantity) { Environment.Exit(0); } // Not enough in stock to purchase that amount
+            //}
         }
 
         // Remove an item from the cart
