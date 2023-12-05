e058b5912bbb51563c4874dfd4cef9e0cb22ef28
Patbu13 <70173620+Patbu13@users.noreply.github.com>
2023-12-05 12:44:57 -0500

    cleaned up Users + Item C/IS/S files

--- CSharpestServer/Services/Interfaces/IUsersService.cs
+++ CSharpestServer/Services/Interfaces/IUsersService.cs
@@ -8,7 +8,5 @@ namespace CSharpestServer.Services.Interfaces
         public Task AddAsync(User user); // Creates user
         public Task DeleteUser(Guid id); // Delete user
         Task<IEnumerable<User>> GetAllAsync(); // see all users in db
-        Task<User?> GetByIdAsync(Guid id); // get a user async
-        User? GetById(Guid id); // get a user
     }
 }
