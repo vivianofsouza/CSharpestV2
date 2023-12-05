20007efac81182111becc00cc5b4a74f45588640
Patbu13 <70173620+Patbu13@users.noreply.github.com>
2023-12-05 00:27:08 -0500

    Lots of cleaning / fixing of User Controller stuff + services, need to do much more formatting across Controllers and services but it's late

--- CSharpestServer/Services/Interfaces/IUsersService.cs
+++ CSharpestServer/Services/Interfaces/IUsersService.cs
@@ -4,6 +4,11 @@ namespace CSharpestServer.Services.Interfaces
 {
     public interface IUsersService
     {
-
+        Task Login(string email, string password); //Login user
+        public Task AddAsync(User user); // Creates user
+        public Task DeleteUser(Guid id); // Delete user
+        Task<IEnumerable<User>> GetAllAsync(); // see all users in db
+        Task<User?> GetByIdAsync(Guid id); // get a user async
+        User? GetById(Guid id); // get a user
     }
 }
--- CSharpestServer/Services/UsersService.cs
+++ CSharpestServer/Services/UsersService.cs
@@ -1,5 +1,7 @@
 ﻿using CSharpestServer.Models;
 using CSharpestServer.Services.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSharpestServer.Services
 {
@@ -11,7 +13,87 @@ namespace CSharpestServer.Services
         {
             _storeContext = storeContext;
         }
+        private User GetInitialisedId(User user)
+        {
+            if (user.Id == Guid.Empty) { user.Id = Guid.NewGuid(); }
 
+            return user;
+        }
+        public Task AddAsync(User user)
+        {
+            try
+            {
+                user = GetInitialisedId(user);
+                user.CartId = user.Id;
+                _storeContext.users.Add(user);
+                _storeContext.SaveChanges();
+
+                Cart cart = new Cart(user);
+                _storeContext.carts.Add(cart);
+                _storeContext.SaveChanges();
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Login(string email, string password)
+        {
+            var users = _storeContext.users.AsEnumerable();
+            try
+            {
+                User found = _storeContext.users.Find(email);
+                if (found.Password == password)
+                {
+                    return Task.CompletedTask;
+                } else
+                {
+                    //wrong password
+                    return Task.FromException(new NullReferenceException());
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        public Task DeleteUser(Guid id)
+        {
+            try
+            {
+                var user = _storeContext.users.Find(id);
+                _storeContext.users.Remove(user);
+                _storeContext.SaveChanges();
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }       
+
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+                return Task.FromResult(_storeContext.users.AsEnumerable());
+        }
+
+        public Task<User?> GetByIdAsync(Guid id)
+        {
+            User? user = _storeContext.users.Find(id);
+            return Task.FromResult(user);
+        }
+
+        public User? GetById(Guid id)
+        {
+            return _storeContext.users.Find(id);
+        }
 
     }
 }
